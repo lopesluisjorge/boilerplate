bef63dd0a7254347dfda816921c23a1e09915f59
Jorge Amorim <lopesluisjorge@hotmail.com>
2025-11-30 15:56:34 -0300

    configure testing-library and add tests to pre-commit

---
+++ src/components/Main/test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import Main from '.';
+
+describe('<Main/>', () => {
+  it('should render the heading', () => {
+    const { container } = render(<Main />);
+    expect(screen.getByRole('heading', { name: /test/i })).toBeInTheDocument();
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      <h1>
+        test
+      </h1>
+    `);
+  });
+});
