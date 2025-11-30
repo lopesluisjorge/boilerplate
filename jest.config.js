ded68ace9d2df559300a981eb911a4ff0d607eb2
Jorge Amorim <lopesluisjorge@hotmail.com>
2025-11-30 17:22:02 -0300

    configure jest styled components

--- jest.config.js
+++ jest.config.js
@@ -6,10 +6,17 @@ module.exports = {
     'src/**/*.ts(x)?',
     '!src/app/*.tsx',
     '!src/lib/registry.tsx',
+    '!src/lib/registry.tsx',
+    '!src/types/**',
+    '!src/styles/**',
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
   },
-};
+  moduleNameMapper: {
+    '^styled-components':
+      'styled-components/dist/styled-components.browser.cjs.js',
+  },
+}
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -1,19 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import Main from '.';
+import { render, screen } from '@testing-library/react'
+import Main from '.'
 
 describe('<Main/>', () => {
   it('should render the heading', () => {
-    const { container } = render(<Main />);
-    expect(screen.getByRole('heading', { name: /test/i })).toBeInTheDocument();
+    const { container } = render(<Main />)
+    expect(screen.getByRole('heading', { name: /test/i })).toBeInTheDocument()
 
-    expect(container.firstChild).toMatchInlineSnapshot(`
-      <main
-        class="sc-bRKDuR cQIcRK"
-      >
-        <h1>
-          test
-        </h1>
-      </main>
-    `);
-  });
-});
+    expect(container.firstChild).toHaveStyle({
+      'background-color': '#06092b',
+    })
+  })
+})
